9afb12507c6d1f51eb39c0febf1e514dafde2663
Pawel <kurowski.pawel1@wp.pl>
2015-06-07 14:05:19 +0200

    poprawione znakowanie obrazka

--- projekt.hs
+++ projekt.hs
@@ -3,6 +3,12 @@ import System.Environment
 
 -- Puzzle (Columns) (Rows)
 data Puzzle = Puzzle [[Int]] [[Int]] deriving Show
+
+{- Picture to główna struktura reprezentująca obrazek
+"x" - nic nie jest postawione
+" " - nie można postawić w tym miejscu
+"*" - jest postawiony znak
+-}
 type Picture = [[Char]]
 
 rInt :: String -> [[Int]]
@@ -22,7 +28,7 @@ initPicture 0 _ = []
 initPicture n m = (initRow m) : (initPicture (n - 1) m)
 			where
 				initRow 0 = []
-				initRow m = ' ' : (initRow (m - 1))
+				initRow m = 'x' : (initRow (m - 1))
 				
 -- przyklad odpalenia programu:
 -- :main "examples/5x5_psi.txt"
